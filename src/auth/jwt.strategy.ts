8c5d121a55fd6e1ef85cabfe14144d17a0616ffa
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-01-11 15:28:28 -0300

    AuthGuard integration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,12 +3,14 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { PassportStrategy } from '@nestjs/passport'
 import { jwtConstants } from "./jwt.constant";
 
-
+// Injecting the strategy
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
         super({
+            // Extraction function, it does it automatically, don't need to create the functions
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            // validates the secret and checks if the token is from the backend, forbidds external injection.
             ignoreExpiration: false,
             secretOrKey: jwtConstants.secret,
         })
